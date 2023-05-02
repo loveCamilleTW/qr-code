7e7f8b4593ebd90943686aa2d7cecbadf7a5d383
loveCamilleTW <yoyopetergod@gmail.com>
2023-05-03 01:07:38 +0800

    Chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import QR_CODE from './images/image-qr-code.png';
+
+function App() {
+  return (
+    <main>
+      <section>
+        <img src={QR_CODE} />
+        <h1>Improve your front-end skills by building projects</h1>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </section>
+    </main>
+  )
+}
+
+export default App
