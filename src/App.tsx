a9388ca9cd1018c0ad6c0e33983244fc588798b5
loveCamilleTW <yoyopetergod@gmail.com>
2023-05-03 12:14:30 +0800

    Style: update styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import QR_CODE from './images/image-qr-code.png';
 function App() {
   return (
     <main>
-      <section>
-        <img src={QR_CODE} />
+      <div className='card'>
+        <img src={QR_CODE} alt='QR code'/>
         <h1>Improve your front-end skills by building projects</h1>
         <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
-      </section>
+      </div>
     </main>
   )
 }
